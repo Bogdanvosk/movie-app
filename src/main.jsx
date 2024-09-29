5ae12e02d48330b7bbd701ebf035ef29819d49c4
Bogdan <bogdanvoskanov@gmail.com>
2024-09-29 14:07:42 +0300

    Fix styles: restyling for inputs and buttons

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import store from './store';
+
+import App from './App.jsx';
 
 import 'react-loading-skeleton/dist/skeleton.css';
 import './styles/globals.scss';
